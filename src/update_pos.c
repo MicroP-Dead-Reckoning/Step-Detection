eea15e2a65e39cc0330b3b50c85ce2c2ae0f7d37
Stephen carter <stephen.carter@mail.mcgill.ca>
2015-11-30 16:27:01 -0500

    added some update position functions

--- src/update_pos.c
+++ src/update_pos.c
@@ -1,14 +1,19 @@
 #include "update_pos.h"
 
 void updatePos(){
-	distance += STEP_SIZE;
+	distance =+ STEP_SIZE;
 }
 
-int sendPos(int direction){
+int sendPos(uint8_t direction){
 	if (dist_last == distance)
 		return 0;
 	
 	dist_last = distance;
+	dir_last = direction;
 	
-
+	uint8_t dist_send[2] = {distance >> 8, (distance & 0x00FF)};
+	
+	CC2500_Write(dist_send, 	CC2500_FIFO_REG, 2);
+	CC2500_Write(&direction, CC2500_FIFO_REG, 1);
+	return 1;
 }
--- src/update_pos.h
+++ src/update_pos.h
@@ -3,13 +3,15 @@
 #include "stm32f4xx.h"                  		// Device header
 #include "stm32f4xx_conf.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "cc2500.h"
 #include "gyroscope.h"
 
 #define STEP_SIZE	1		//cm
 
-int distance = 0;
-int dist_last = 0;
-int dir_last = 0;
+uint16_t distance = 0;
+uint16_t dist_last = 0;
+uint8_t dir_last = 0;
 
 void UpdatePos(void const *arguments);
+int sendPos(uint8_t direction);
