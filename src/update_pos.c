64f7b1cd64922a88a3f70a1ea52e0477533a4da5
Jit <kanetkarster@gmail.com>
2015-12-04 16:29:20 -0500

    began testing

--- src/update_pos.c
+++ src/update_pos.c
@@ -1,12 +1,15 @@
 #include "update_pos.h"
 #include <stdio.h>
+#include "osObjects.h"
 #include "cc2500.h"
 
 #define TOTAL_STEPS 100
+uint8_t DONE[3] = {69, 69, 69};
 
 uint8_t direction[TOTAL_STEPS];
 uint8_t distance[TOTAL_STEPS];
 int8_t steps = -1;
+
 
 /*!
  Function handles the updating of distance (number of steps)
@@ -26,9 +29,10 @@ void update_pos(uint8_t new_direction){
 int send_pos(){
 	for (int i = 0; i <= steps; i++) {
 		printf("%d: dir: %d, dist: %d\n", i, direction[i], distance[i]);
+		CC2500_Write(&direction[i], CC2500_FIFO_REG, 1);
+		CC2500_Write(&distance[i], CC2500_FIFO_REG, 1);
+		osDelay(50);
 	}
-	
-	CC2500_Write(direction, CC2500_FIFO_REG, 1);
-	CC2500_Write(distance, CC2500_FIFO_REG, 1);
+	CC2500_Write(DONE, CC2500_FIFO_REG, 3);
 	return 0;
 }
